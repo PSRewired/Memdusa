bb1392037dc35710ca10dcd55fb68875d767f070
Zero1UP <Zero1UP@gmail.com>
2026-05-26 10:21:10 -0600

    SOCOM II PAL Remote payload

--- Memdusa.Core/Constants/AppIds.cs
+++ Memdusa.Core/Constants/AppIds.cs
@@ -2,14 +2,15 @@ namespace Memdusa.Core.Constants;
 
 public enum AppIds
 {
-    ATV_Offroad_Fury_3_NTSC = 10334,
-    KillZone_NTSC = 10724,
-    Killzone_PAL = 10444,
-    Ratchet_and_Clank_Up_Your_Arsenal_NTSC = 10684,
-    Ratchet_and_Clank_DeadLocked_NTSC = 11184,
-    Socom_2_NTSC = 10472,
-    Socom_3_NTSC = 20004,
-    Socom_Combined_Assault_NTSC = 20604,
-    Syphon_Filter_The_Omega_Strain_NTSC = 10411,
-    Syphon_Filter_The_Omega_Strain_PAL = 10414,
+    ATV_Offroad_Fury_3_NTSC = 10334, //SCUS_974.05
+    KillZone_NTSC = 10724, //SCUS_974.02
+    Killzone_PAL = 10444, //SCES_520.04
+    Ratchet_and_Clank_Up_Your_Arsenal_NTSC = 10684, //SCUS_973.53
+    Ratchet_and_Clank_DeadLocked_NTSC = 11184, //SCUS_974.65
+    Socom_2_NTSC = 10472, //SCUS_972.75
+    Socom_2_PAL = 10481, //SCES_519.04
+    Socom_3_NTSC = 20004, //SCUS_974.74
+    Socom_Combined_Assault_NTSC = 20604, //SCUS_975.45
+    Syphon_Filter_The_Omega_Strain_NTSC = 10411, //SCUS_972.64
+    Syphon_Filter_The_Omega_Strain_PAL = 10414, //SCES_520.33
 }
