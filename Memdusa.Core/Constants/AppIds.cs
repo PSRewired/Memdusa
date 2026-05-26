d53afeca7705a36e5cf9e405b4577e5da159155e
Zero1UP <Zero1UP@gmail.com>
2026-05-26 03:41:33 -0600

    Forgot to include the appid updates

--- Memdusa.Core/Constants/AppIds.cs
+++ Memdusa.Core/Constants/AppIds.cs
@@ -4,6 +4,7 @@ public enum AppIds
 {
     ATV_Offroad_Fury_3_NTSC = 10334,
     KillZone_NTSC = 10724,
+    Killzone_PAL = 10444,
     Ratchet_and_Clank_Up_Your_Arsenal_NTSC = 10684,
     Ratchet_and_Clank_DeadLocked_NTSC = 11184,
     Socom_2_NTSC = 10472,
